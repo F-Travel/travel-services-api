0945df1038a393823c7bf9ebade633106eeaaf8a
Pham Trung Kien <kienpt375@gmail.com>
2022-11-19 01:05:36 +0700

    KienPT commit

--- src/main/java/com/fptu/edu/travelservices/config/Helper.java
+++ src/main/java/com/fptu/edu/travelservices/config/Helper.java
@@ -3,6 +3,9 @@ package com.fptu.edu.travelservices.config;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Helper {
@@ -49,5 +52,28 @@ public class Helper {
             digest = "";
         }
         return digest;
+    }
+
+    public static String hmacSHA512(final String key, final String data) {
+        try {
+
+            if (key == null || data == null) {
+                throw new NullPointerException();
+            }
+            final Mac hmac512 = Mac.getInstance("HmacSHA512");
+            byte[] hmacKeyBytes = key.getBytes();
+            final SecretKeySpec secretKey = new SecretKeySpec(hmacKeyBytes, "HmacSHA512");
+            hmac512.init(secretKey);
+            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+            byte[] result = hmac512.doFinal(dataBytes);
+            StringBuilder sb = new StringBuilder(2 * result.length);
+            for (byte b : result) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString();
+
+        } catch (Exception ex) {
+            return "";
+        }
     }
 }
--- src/main/java/com/fptu/edu/travelservices/controller/PaymentController.java
+++ src/main/java/com/fptu/edu/travelservices/controller/PaymentController.java
@@ -10,9 +10,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -22,8 +28,8 @@ import java.util.*;
 @RequestMapping("/api/payment")
 public class PaymentController {
 
-    @Value("${host.url}")
-    private String hostUrl;
+//    @Value("${host.url}")
+//    private String hostUrl;
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -36,13 +42,13 @@ public class PaymentController {
         Long orderAmount = Long.parseLong(request.getAmount()) * 100;
 //        String orderDes = request.getParameter("description");
         String orderDes = request.getDescription();
-        Order newOrder = new Order(orderAmount, orderDes);
-        String ipAddr = request.getRemoteAddr();
-        log.info("ip: " + ipAddr);
-        Long createdOrderId = orderService.createOrder(newOrder, ipAddr);
+//        Order newOrder = new Order(orderAmount, orderDes);
+//        String ipAddr = request.getRemoteAddr();
+//        log.info("ip: " + ipAddr);
+//        Long createdOrderId = orderService.createOrder(newOrder, ipAddr);
 
 //        Query Parameter
-        String vnp_Version = "2.0.1";
+        String vnp_Version = "2.1.0";
 
         String vnp_Command = "pay";
 
@@ -50,26 +56,35 @@ public class PaymentController {
 
         String vnp_Amount = orderAmount.toString();
 
-        String vnp_BankCode = request.getBankCode();
+//        String vnp_BankCode = request.getBankCode();
 
-        LocalDateTime time = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        String vnp_CreateDate = time.format(formatter);
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+
+//        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        //Add Params of 2.0.1 Version
+//        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 
         String vnp_CurrCode = "VND";
 
-        String vnp_IpAddr = ipAddr;
+        String vnp_IpAddr = "13.160.92.202";
 
         String vnp_Locale = "vn";
 
         String vnp_OrderInfo = orderDes;
 
-        String vnp_OrderType = "billpayment";
+        String vnp_OrderType = "other";
 
 //        String vnp_ReturnUrl = Config.vnp_Returnurl;
-        String vnp_ReturnUrl = hostUrl + "return";
+        String vnp_ReturnUrl = "https://f-travel.azurewebsites.net/api/feedback/list-report";
 
-        String vnp_TxnRef = createdOrderId.toString();
+//        String vnp_TxnRef = createdOrderId.toString();
+        String vnp_TxnRef = "1225";
 //        String vnp_TxnRef = Helper.getRandomNumber(8);
 
         Map<String, String> params = new HashMap<>();
@@ -78,8 +93,9 @@ public class PaymentController {
         params.put("vnp_Command", vnp_Command);
         params.put("vnp_TmnCode", vnp_TmnCode);
         params.put("vnp_Amount", vnp_Amount);
-        params.put("vnp_BankCode", vnp_BankCode);
+        params.put("vnp_BankCode", "NCB");
         params.put("vnp_CreateDate", vnp_CreateDate);
+        params.put("vnp_ExpireDate", vnp_ExpireDate);
         params.put("vnp_CurrCode", vnp_CurrCode);
         params.put("vnp_IpAddr", vnp_IpAddr);
         params.put("vnp_Locale", vnp_Locale);
@@ -95,12 +111,12 @@ public class PaymentController {
         Iterator itr = fieldNames.iterator();
         while (itr.hasNext()) {
             String fieldName = (String) itr.next();
-            String fieldValue = params.get(fieldName);
+            String fieldValue = (String) params.get(fieldName);
             if ((fieldValue != null) && (fieldValue.length() > 0)) {
                 //Build hash data
                 hashData.append(fieldName);
                 hashData.append('=');
-                hashData.append(fieldValue);
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
                 //Build query
                 query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
                 query.append('=');
@@ -112,12 +128,36 @@ public class PaymentController {
             }
         }
         String queryUrl = query.toString();
-        String vnp_SecureHash = Helper.Sha256(PaymentConfig.vnp_HashSecret + hashData);
-        log.info("Hash " + vnp_SecureHash);
+
+        String vnp_SecureHash = Helper.hmacSHA512(PaymentConfig.vnp_HashSecret, hashData.toString());
         queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
 
+//        String vnp_SecureHash = Helper.hmacSHA512(PaymentConfig.vnp_HashSecret , hashData.toString());
+//        log.info("Hash " + vnp_SecureHash);
+//        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+
+//        String paymentUrl = PaymentConfig.vnp_PayUrl + '?' + queryUrl;
         String paymentUrl = PaymentConfig.vnp_PayUrl + '?' + queryUrl;
         log.info("Paymenr url: " + paymentUrl);
+
+//        URL url = new URL(paymentUrl);
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        connection.setRequestMethod("GET");
+//        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        String inputLine;
+//        StringBuilder response = new StringBuilder();
+//
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//        String Rsp = response.toString();
+//        String respDecode = URLDecoder.decode(Rsp, "UTF-8");
+//        String[] responseData = respDecode.split("&|\\=");
+//        com.google.gson.JsonObject job = new JsonObject();
+//        job.addProperty("data", Arrays.toString(responseData));
+//        Gson gson = new Gson();
+//        resp.getWriter().write(gson.toJson(job));
 
 //        return "redirect:" + paymentUrl;
         return ResponseEntity
